67474501c99854d3261b7e6329af11d9b0d81356
loicbourgois <loic@bourgois.pro>
2018-02-01 23:03:16 +0000

    test x100

--- test/specs/scale.financialLinear.tests.js
+++ test/specs/scale.financialLinear.tests.js
@@ -7,29 +7,31 @@ describe('FinancialLinear Scale', function() {
 	});
 
 	it('Should have the correct min/max values', function() {
-		var chart = window.acquireChart({
-			type: 'candlestick',
-			data: {
-				datasets: [{
-					yAxisID: 'yScale0',
-					data: [
-						{t: '2017-04-01', o: 70, h: 100, l: 30, c: 50},
-						{t: '2017-04-01', o: 60, h: 200, l: 40, c: 90},
-					]
-				}]
-			},
-			options: {
-				scales: {
-					yAxes: [{
-						id: 'yScale0',
-						type: 'financialLinear'
+		for(var c = 0 ; c < 100 ; c++) {
+			var chart = window.acquireChart({
+				type: 'candlestick',
+				data: {
+					datasets: [{
+						yAxisID: 'yScale0',
+						data: [
+							{t: '2017-04-01', o: 70, h: 100, l: 30, c: 50},
+							{t: '2017-04-01', o: 60, h: 200, l: 40, c: 90},
+						]
 					}]
+				},
+				options: {
+					scales: {
+						yAxes: [{
+							id: 'yScale0',
+							type: 'financialLinear'
+						}]
+					}
 				}
-			}
-		});
+			});
 
-		expect(chart.scales.yScale0.max).toBe(220);
-		expect(chart.scales.yScale0.min).toBe(20);
+			expect(chart.scales.yScale0.max).toBe(220);
+			expect(chart.scales.yScale0.min).toBe(20);
+		}
 	});
 
 });
